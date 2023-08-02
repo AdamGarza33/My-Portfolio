7e92ae76ce2748c45a0dac1b6b9271274438751d
Addit <adam.garza33@hotmail.com>
2023-08-02 14:47:08 -0500

    added content that will go into navbar

--- my-portfolio/src/components/header/Header.jsx
+++ my-portfolio/src/components/header/Header.jsx
@@ -10,13 +10,35 @@ const Header = () => {
                 <div className='nav__menu'>
                     <ul className='nav__list grid'>
                         <li className='nav__item'>
-                            <a href='' className='nav__link'>
-                                <i className="uil uil- nav__icon"></i>
+                            <a href='#home' className='nav__link'>
+                                <i className="uil uil- nav__icon"></i> Home
+                            </a>
+                        </li>
+
+                        <li className='nav__item'>
+                            <a href='#about' className='nav__link'>
+                                <i className="uil uil- nav__icon"></i> About
+                            </a>
+                        </li>
+
+                        <li className='nav__item'>
+                            <a href='#projects' className='nav__link'>
+                                <i className="uil uil- nav__icon"></i> Projects
+                            </a>
+                        </li>
+
+                        <li className='nav__item'>
+                            <a href='#contact' className='nav__link'>
+                                <i className="uil uil- nav__icon"></i> Contact
                             </a>
                         </li>
                     </ul>
+
+                    
                 </div>
             </nav>
         </header>
     )
 }
+
+export default Header;
