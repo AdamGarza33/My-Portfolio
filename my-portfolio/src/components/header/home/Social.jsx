5dd991d4ec5d43742be9fb52960c4b209076a867
Addit <adam.garza33@hotmail.com>
2023-08-03 14:49:59 -0500

    created Home component w/ Social and Data sections, added Data component w/ home__data section

---
+++ my-portfolio/src/components/header/home/Social.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Social = () => {
+    return (
+      <div className='home__social'>
+        <a href='https://www.linkedin.com/in/adam-garza-180710268/' className='home__social-icon' target='_blank'>
+        <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href='https://github.com/AdamGarza33' className='home__social-icon' target='_blank'>
+        <i class="uil uil-github"></i>
+        </a>
+
+        <a href='' className='home__social-icon' target='_blank'>
+        <i class="uil uil-dribbble"></i>
+        </a>
+
+      </div>  
+    )
+}
+
+export default Social;
